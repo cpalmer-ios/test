4726c0d151d1454070892f2d09bcbb3aedee4912
cpalmer-ios <write.to.chris.palmer@gmail.com>
2023-07-20 17:33:26 +0100

    search algorithim

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -3,24 +3,42 @@ import { useSession } from "next-auth/react"
 import Layout from "../components/layout"
 import AccessDenied from "../components/access-denied"
 import Table from "../components/Table"
+import SearchBar from "../components/SearchBar"
+import { useRouter } from "next/router"
 
 export default function ProtectedPage() {
   const { data: session } = useSession()
   const [content, setContent] = useState()
   const [data, setData] = useState<any[]>([])
 
+  const fetchData = async () => {
+    const res = await fetch("/api/odds/protected")
+    const json = await res.json()
+    if (json.content) {
+      setContent(json.content)
+    }
+    if(json.data) {
+      setData(json.data)
+    }
+  }
+
+
+  const handleSearch = (e: string) => {
+
+    console.log(e)
+
+    if(e === '') {
+      fetchData()
+    }
+
+    const res = data.filter(name => name.competition.includes(e))
+
+    // const res = data.filter((item) => item.competition.toLowerCase() == e.toLowerCase())
+    setData(res)
+  }
+
   // Fetch content from protected route
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/api/odds/protected")
-      const json = await res.json()
-      if (json.content) {
-        setContent(json.content)
-      }
-      if(json.data) {
-        setData(json.data)
-      }
-    }
     fetchData()
   }, [session])
   
@@ -37,6 +55,7 @@ export default function ProtectedPage() {
   // If session exists, display content
   return (
     <Layout>
+      <SearchBar onSubmit={(e) => handleSearch(e)} />
       <h1>Protected Odds Page</h1>
         <strong>{content ?? "\u00a0"}</strong>
       <Table data={data} />
