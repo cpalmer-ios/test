e3b3b15e6c3545fb1f02e8ea377a1b01afe763ae
cpalmer-ios <write.to.chris.palmer@gmail.com>
2023-07-20 16:56:36 +0100

    handle bookmaker view click

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,13 +1,16 @@
 import TableItem from "./TableItem";
+import styles from "./table.module.css"
 
 export default function Table({ data }: any): any {
 
   return (
-    <div>
+    <table className={styles.table}>
+        <tbody className={styles.tbody}>
       {data &&
         data.map((item: any, index: number) => (
           <TableItem item={item} key={index} competition={item.competition}/>
         )).sort((item: any) => item.competition === item.competition)}
-    </div>
+        </tbody>
+    </table>
   );
 }
--- components/TableItem.tsx
+++ components/TableItem.tsx
@@ -1,21 +1,44 @@
+import styles from "./table.module.css";
+
 export default function TableItem({
   item,
   index,
-  bookmaker,
   competition,
 }: any): any {
-  console.log(item);
+  
+  const handleViewClick = (e: any) => {
+    e.preventDefault();
+    console.log(e.target.value)
+  }
+
   return (
-    <div key={index}>
-      {/* <div>{item.fixture_id}</div> */}
-      <h3>{competition}</h3>
-      <div className="">
-        <p>Country: {item.country_name}</p>
-        <p>
-          {item.home} vs {item.away}
-        </p>
-        <p>Start Time:{item.start_time}</p>
-      </div>
-    </div>
+    <td className={styles.tableContainer} key={index}>
+      <h4 className={styles.tableTitle}>{competition}</h4>
+      <tr className={styles.td}>
+        <td>
+          <p>
+            <strong>{item.home}</strong> vs <strong>{item.away}</strong> -{" "}
+            {item.country_name}
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <p>
+            {new Date(item.start_time).toLocaleDateString("en-us", {
+              weekday: "long",
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </p>
+        </td>
+      </tr>
+      <tr>
+        <button value={item.fixture_id} onClick={(e) => handleViewClick(e)} className={styles.tableButton}>VIEW</button>
+      </tr>
+    </td>
   );
 }
